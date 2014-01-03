539cfb88acbfcc875e41d26b0cf5609dce0e0e88
Joe Maldonado <joe@bowfusion.com>
2014-01-02 19:02:10 -0500

    Initial skeleton

---
+++ main.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <unistd.h>
+
+using namespace std;
+
+int main() {
+  // Instantiate command processro
+  // Instantiate alarms class
+  // Instantiate tasks class
+  // Instantiate controls class
+
+  while (true) {
+    // Evaluate commands
+    // Evaluate alarm events
+    // Evaluate task events
+    // Evaluate control events
+    sleep(10);
+  }
+  return 0;
+}
