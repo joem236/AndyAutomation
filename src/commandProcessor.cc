c4eb572dc27d1c13488f881241569ad4f7f4733c
Baconator252 <andyckmaldonado@gmail.com>
2014-01-06 15:22:00 -0800

    command processor requirements

---
+++ src/commandProcessor.cc
@@ -0,0 +1,7 @@
+/*
+ * Requirements
+ * Accept a sentence as input such as: create an alarm for 05:00:00
+ * Break up the sentence into words
+ * find a way to make a decision on what to do with the individual words and commands
+ */
+ 
